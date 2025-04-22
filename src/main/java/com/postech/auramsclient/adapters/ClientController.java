5c58e6c688a70e47410992a199c92c6dcb7565c5
Jessica Sousa <jessica.persou@gmail.com>
2025-04-22 00:09:29 -0300

    adding test mass for integration with microservices

--- src/main/java/com/postech/auramsclient/adapters/ClientController.java
+++ src/main/java/com/postech/auramsclient/adapters/ClientController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/api/v1/clients")
 public class ClientController {
 
     private final CreateClientUseCase createClientUseCase;
